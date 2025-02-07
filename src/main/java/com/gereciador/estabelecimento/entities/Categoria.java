f6cce4504015412264a08a3c842003da5126bc3b
ismaellima4 <ismael.github@gmail.com>
2025-02-07 15:02:36 -0300

    create categoria entity

--- src/main/java/com/gereciador/estabelecimento/entities/Categoria.java
+++ src/main/java/com/gereciador/estabelecimento/entities/Categoria.java
@@ -1,10 +1,26 @@
 package com.gereciador.estabelecimento.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
