f6b34cc9d844f85e820d732c4b814f161e66e458
ismaellima4 <ismael.github@gmail.com>
2025-02-08 13:21:22 -0300

    initializer Interface Repository, Service

---
+++ src/main/java/com/gereciador/estabelecimento/services/Service.java
@@ -0,0 +1,11 @@
+package com.gereciador.estabelecimento.services;
+
+import java.util.List;
+
+public interface Service<E, P> {
+    E save(E obj);
+    E update(P primaryKey, E obj);
+    void delete(P primaryKey);
+    E getById(P primaryKey);
+    List<E> getAll();
+}
