9373dc414bd4eaf39860a424f5a41918e54763ce
ismaellima4 <ismael.github@gmail.com>
2025-03-02 19:20:53 -0300

    add password encoder, Auth manager

--- src/main/java/com/gereciador/estabelecimento/config/SecurityConfig.java
+++ src/main/java/com/gereciador/estabelecimento/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.gereciador.estabelecimento.config;
 
+import com.gereciador.estabelecimento.enums.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,10 +24,24 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/categorias").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/categorias/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/categorias", "/forncedores", "/produtos").hasAnyRole(UserRole.ADMIN_ESTOQUE.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.PATCH, "/categorias/{id}","/fornecedores/{id}", "/produtos/{id}").hasAnyRole(UserRole.ADMIN_ESTOQUE.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE, "/categorias/{id}","/fornecedores/{id}", "/produtos/{id}").hasAnyRole(UserRole.ADMIN_ESTOQUE.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/pedidos", "/pagamentos", "/clientes").hasAnyRole(UserRole.ADMIN_CAIXA.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.PATCH, "/pedidos/{id}","/pagamentos/{id}", "/clientes/{id}").hasAnyRole(UserRole.ADMIN_CAIXA.name(), UserRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE, "/pedidos/{id}","/pagamentos/{id}", "/clientes/{id}").hasAnyRole(UserRole.ADMIN_CAIXA.name(), UserRole.ADMIN.name())
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
